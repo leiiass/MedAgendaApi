a76b68a91c3ad768758000767d4b6f02e998bc1e
leiiass <leiasoares2001@gmail.com>
2025-04-15 18:38:08 -0300

    Definido modelos

---
+++ src/MedAgenda.Dominio/Modelos/Consulta.cs
@@ -0,0 +1,16 @@
+﻿namespace MedAgenda.Dominio.Modelos
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+
+        public int PacienteId { get; set; }
+        public Paciente Paciente { get; set; }
+
+        public int MedicoId { get; set; }
+        public Medico Medico { get; set; }
+
+        public DateTime DataHora { get; set; }
+        public string Status { get; set; } // Ex: Agendada, Cancelada, Realizada
+    }
+}
---
+++ src/MedAgenda.Dominio/Modelos/Especialidade.cs
@@ -0,0 +1,8 @@
+﻿namespace MedAgenda.Dominio.Modelos
+{
+    public class Especialidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
